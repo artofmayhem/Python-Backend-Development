4dc55c3b1ebac470656696ae02d407b46fb4d0f2
artofmayhem <chef_tony_miller@yahoo.com>
2021-10-23 09:47:44 -1000

    small chages to server

--- Backend Python/CodeRepo/BasicServer/basicServer.py
+++ Backend Python/CodeRepo/BasicServer/basicServer.py
@@ -6,6 +6,7 @@ hostName = "localhost"
 serverPort = 8080
 time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+
 class Server(BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -15,7 +16,7 @@ class Server(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<img><head><title>Basic Python Server</title></head>", "utf-8"))
         self.wfile.write(bytes("<h1>Welcome to the Matrix, Neo</h1>", "utf-8"))
         self.wfile.write(bytes("<h3>Current time: %s</h3>" % time, "utf-8"))
-        self.wfile.write(bytes("<p>You're up late tonight</p>", "utf-8"))
+        # this line is a routed to the /zion page. TBD
         self.wfile.write(bytes('<button><a href="/zion">Take the Red Pill...</a></button>', 'utf-8'))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
