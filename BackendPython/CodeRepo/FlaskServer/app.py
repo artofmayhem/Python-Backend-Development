f903fe8dc2cb9a8abe7240517d404eb66abb0ab5
artofmayhem <chef_tony_miller@yahoo.com>
2021-12-07 00:41:39 -1000

    images added

--- BackendPython/CodeRepo/FlaskServer/app.py
+++ BackendPython/CodeRepo/FlaskServer/app.py
@@ -15,11 +15,27 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+# Routes
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+@app.route('/platforms')
+def platforms():
+    return render_template('platforms.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
